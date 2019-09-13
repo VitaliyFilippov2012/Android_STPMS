7a40ff9c5cdd99d3adc737c6032070156cd4bcc8
Vitaliy <vitaliy.filippov.2012@mail.ru>
2019-09-13 08:29:47 +0300

    lab 1

---
+++ Lab1/app/src/main/java/filippovvitaliyleonidovich/bstu/fit/lab1/Filippov/FilippovClass.java
@@ -0,0 +1,8 @@
+package filippovvitaliyleonidovich.bstu.fit.lab1.Filippov;
+
+public class FilippovClass {
+
+    public String getValue(){
+        return "Hello world";
+    }
+}
--- Lab1/app/src/main/java/filippovvitaliyleonidovich/bstu/fit/lab1/MainActivity.java
+++ Lab1/app/src/main/java/filippovvitaliyleonidovich/bstu/fit/lab1/MainActivity.java
@@ -3,9 +3,10 @@ package filippovvitaliyleonidovich.bstu.fit.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    //TODO HELLO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
